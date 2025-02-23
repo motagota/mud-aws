0f2ffa2dd660d7dcf1a23541d92adb9cb3402842
James Shoesmith <motagota@gmail.com>
2025-02-23 15:13:40 +1000

    add web socket connection listener

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { createServer } from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import path from "path";
 
 const app = express();
@@ -13,6 +13,10 @@ const io = new Server(http, {
 });
 
 app.use(express.static(path.join(__dirname, "../public")));
+
+io.on("connection", (socket: Socket) => {
+    console.log("Player connected:", socket.id);
+});
 
 const PORT = process.env.PORT || 3030;
 http.listen(PORT, () => {
