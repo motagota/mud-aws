5cd65c23658a0ee474c5b538ea764434bd78c2c3
James Shoesmith <motagota@gmail.com>
2025-02-23 14:52:39 +1000

    add ts

---
+++ dist/server.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const http = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(http, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
+const PORT = process.env.PORT || 3030;
+http.listen(PORT, () => {
+    console.log(`Chat Server running on port ${PORT}`);
+});
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from "path";
+
+const app = express();
+const http = createServer(app);
+const io = new Server(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+const PORT = process.env.PORT || 3030;
+http.listen(PORT, () => {
+  console.log(`Chat Server running on port ${PORT}`);
+});
